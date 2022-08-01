9c59d4a2a63e3a57a488d9c882d19ea9bb5f073a
André Gonçalves Araújo <85709695+agaraujo2021@users.noreply.github.com>
2022-08-01 17:42:53 -0300

    Update update_repo.sh

--- update_repo.sh
+++ update_repo.sh
@@ -6,23 +6,30 @@ DIR="/repos/dev.azure.com/Extreme-Digital/0160_RJ_PRODERJ_RH/_git/"
 sync(){
  cd /repos
  ls -lC1 $DIR > /tmp/repos.txt
- INPUT="/tmp/repos.txt"
-
+ INPUT="/tmp/repos.txt" 
+ 
  while read -r reponame
- do
+ do  
     cd $DIR/$reponame
     EXISTS=$(git remote show|grep $reponame |wc -l)
     if [ $EXISTS -eq 0 ]; then
-          #git remote add $reponame http://$USERNAME:$USERPW1@www.gitlab.proderj.rj.gov.br:8081/rj-digital/$reponame
-          #git push --all $reponame
-          echo "Remote repo not exists"
+          echo "Remote repo not exists."
+          git remote add $reponame http://$USERNAME:$USERPW@www.gitlab.proderj.rj.gov.br:8081/rj-digital/$reponame
+          git push --all $reponame
+          echo "Remote repo created and updated."
         else
-           echo "Remote repo exists!"
+          echo "Remote repo exists, start updating process!"
+          git push --all $reponame
+          if [ $? -eq 0 ];then
+             echo "Repository updated."
+          else
+             echo "Error whilst update data to Gitlab."
+          fi
         fi
  done < "$INPUT"
 }
 
 while true; do
   sync
-  sleep 30
+  sleep 960 
 done
